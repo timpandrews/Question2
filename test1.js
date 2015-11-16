d1db5ecdaf682c5a2de2851898e4547fb34bfcc5
Tim Andrews <tim.andrews@imagineifinc.com>
2015-11-16 15:43:31 -0500

    ema

--- test1.js
+++ test1.js
@@ -4,10 +4,10 @@ function init() {
 
     //alert("hello")
     var i = 1;
-    var maxIterations = 15; //******** Set to 100 when done ********
+    var maxIterations = 22; //******** Set to 100 when done ********
 
     var randomNumberArray = []
-    var averageArray = []
+    var EMAArray = []
 
     var repeater = setInterval(function () {
         if (i <= maxIterations) {
@@ -16,34 +16,43 @@ function init() {
             var randomNumber = getRandomNumber()
             randomNumberArray.push(randomNumber)
 
-            var average = getAverage(randomNumberArray)
-            averageArray.push(average)
+            var EMA = getEMA(randomNumberArray, EMAArray)
+            EMAArray.push(EMA)
             
-            buildTable(randomNumberArray, averageArray)
+            buildTable(randomNumberArray, EMAArray)
 
             i++;
         } else {
             //alert("done")
             clearInterval(repeater);
         }
-    }, 1000);
+    }, 500);
 
 }
 
-function getAverage(randomNumberArray) {
+function getEMA(randomNumberArray, EMAArray) {
 
-    var sum = 0
-    var average
-
-    for (var i = 0; i < randomNumberArray.length; i++) {
-        
-        sum += randomNumberArray[i]  
-
+    /* Exponential Move Average (EMA)
+        Current EMA= ((Price(current) - previous EMA)) X multiplier) + previous EMA.
+        Source: http://www.investopedia.com/articles/trading/10/simple-exponential-moving-averages-compare. asp
+    */
+    
+    var EMA
+    var previousEMA
+    var sampleSize = 20
+    var smoothingFactor = 2 / (1 + sampleSize)
+    var currentValue = randomNumberArray[randomNumberArray.length - 1]
+    
+    //alert(randomNumberArray)
+    //Get 1st EMA with 1st record
+    if (randomNumberArray.length <= 1) {
+        EMA = randomNumberArray[0]
+    } else {
+        previousEMA = EMAArray[randomNumberArray.length-2]
+        EMA = (((currentValue - previousEMA) * smoothingFactor) + previousEMA)
     }
 
-    average = sum / randomNumberArray.length
-
-    return average
+    return EMA
 
 }
 
@@ -57,10 +66,11 @@ function getRandomNumber() {
 
 }
 
-function buildTable(randomNumberArray, averageArray) {
+function buildTable(randomNumberArray, EMAArray) {
 
     //alert(randomNumberArray)
     var arrayLength = randomNumberArray.length
+    var relativeToEMA
     //alert(arrayLength)
     
 
@@ -73,6 +83,8 @@ function buildTable(randomNumberArray, averageArray) {
     html+=          "</thead>"
     html +=     "<tbody>"
 
+    /* Start Loop 10 records from the end (showing last 10 records), unless there 
+       are less then 10 records then start at the 1st record and show all records */
     var startLoop = arrayLength - 10
     if (startLoop < 0) {
         startLoop = 0
@@ -80,10 +92,17 @@ function buildTable(randomNumberArray, averageArray) {
     
     for (x = startLoop ; x < arrayLength; x++) {
 
-        //alert(x)
-        html+=      "<tr>"
+        if (randomNumberArray[x] > EMAArray[x]) {
+            relativeToEMA = "aboveEMA"
+        } else if (randomNumberArray[x] < EMAArray[x]) {
+            relativeToEMA = "belowEMA"
+        } else if (randomNumberArray[x] = EMAArray[x]) {
+            relativeToEMA = "equalToEMA"
+        } 
+        
+        html +=     "<tr class='" + relativeToEMA + "'>"
         html+=          "<td>" + randomNumberArray[x] +"</td>"
-        html +=         "<td>" + Math.round(averageArray[x] * 100) / 100 + "</td>"
+        html +=         "<td>" + Math.round(EMAArray[x] * 100) / 100 + "</td>"
         html+=      "</tr>"
 
     }
